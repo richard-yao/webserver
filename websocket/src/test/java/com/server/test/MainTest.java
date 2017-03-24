6a602cb457685ec6f6939d6d34dfa2366d75c345
richard-yao <yaoyxsgf@gmail.com>
2017-03-24 10:01:13 +0800

    construct complete maven directory

---
+++ websocket/src/test/java/com/server/test/MainTest.java
@@ -0,0 +1,12 @@
+package com.server.test;
+/**
+* @author RichardYao richardyao@tvunetworks.com
+* @date Mar 24, 2017 9:58:35 AM
+*/
+public class MainTest {
+
+	public static void main(String[] args) {
+
+	}
+
+}
